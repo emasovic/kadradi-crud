31859417c146ad49c58cd56dbc01d66d6254a189
stefan <stefanats@hotmail.com>
2018-04-10 14:59:32 +0200

    token function

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Grid, Input, Button } from 'semantic-ui-react';
 import css from './styles.scss';
+import post from '../fetch/post'
 
 class Home extends React.Component {
 	constructor(props) {
@@ -10,12 +11,18 @@ class Home extends React.Component {
 		}
 	}
 	componentWillMount() {
-		localStorage.setItem('myCat', 'Tom');
-
+		if(localStorage.token) {
+			console.log('IMA TOKENA')
+			// AKO IMA PROVERAVAMO SA SERVEROM DA LI JE VALIDAN
+			const response = post.secure('/checkToken',{});
+			if(response.success) {
+				localStorage.setItem('token', response.token)
+			} else {
+				localStorage.removeItem('token')
+			}
+		}
 	}
 	render() {
-		let cat = localStorage.getItem("myCat");
-		console.log('macka', cat)
 		return(
 			<div className={css.wrapper}>
 				<div className={css.holder}>
