1c7e21ab7a84cffc218608e00b8257b07bf36640
stefan <stefanats@hotmail.com>
2018-04-10 14:30:28 +0200

    home style

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {Grid, Input, Button } from 'semantic-ui-react';
+import css from './styles.scss';
 
 class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			s: ''
+		}
+	}
+	componentWillMount() {
+		localStorage.setItem('myCat', 'Tom');
+
+	}
 	render() {
+		let cat = localStorage.getItem("myCat");
+		console.log('macka', cat)
 		return(
-			<div>
-				home
+			<div className={css.wrapper}>
+				<div className={css.holder}>
+						<Input focus placeholder='Username...' style={{marginRight:'10px'}}/>
+						<Input type='password' focus placeholder='Password...' style={{marginRight:'10px'}}/>
+						<Button>Submit</Button>
+				</div>
 			</div>
 		)
 	}
