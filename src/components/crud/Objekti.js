1c9b7c5c7f95ff5459cdda270f374c0b806ea364
elvis <ekobree@live.com>
2018-04-13 12:51:23 +0200

    preparing to pull

--- src/components/crud/Objekti.js
+++ src/components/crud/Objekti.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
-import { Grid, Container, Tab, Button } from 'semantic-ui-react';
+import { Grid, Container, Tab, Button, Dropdown } from 'semantic-ui-react';
 import post from '../fetch/post';
 
 class Objekti extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    }
+  }
   getAllObjCategories = async () => {
-    let categories = await post.secure('/allCategories', {
+    let response = await post.secure('/allCategories', {
       token: this.props.token
     });
-    console.log("CATEGORIES", categories)
+    if(response.token.success) {
+      let categories = response.categories.map(item => {
+        return (
+          {
+            key: item.id,
+            value: item.id,
+            text: item.nameM
+          }
+        )
+      })
+      this.setState({
+        categories
+      })
+    }
+  }
+  handleItemClick = async ( e, { value } )  => {
+    let response = await post.secure('/objectsFromCategories', {
+      token: this.props.token,
+      categoryId: value
+    });
+    console.log("OBJECT REP", response)
   }
   componentWillMount() {
     this.getAllObjCategories()
   }
   render() {
+    console.log("STEJT", this.state)
     return (
       <div style={{ height: '100vh' }}>
+      <Dropdown placeholder='Izaberite kategoriju' 
+      fluid search selection
+      onChange={this.handleItemClick}
+      options={this.state.categories} />
         Ovo su objekti
       </div>
     );
