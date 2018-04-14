1aae3551b0f5307062e9ca82eccd06847466449d
cybele <kicaubuntu@gmail.com>
2018-04-14 15:44:01 +0200

    users delete

--- src/components/crud/users/Korisnici.js
+++ src/components/crud/users/Korisnici.js
@@ -3,39 +3,88 @@ import post from '../../fetch/post';
 import { withRouter } from 'react-router';
 import { Grid, Menu, Tab, Button, Dropdown, Table, Icon } from 'semantic-ui-react';
 
-class Korisnici extends React.Component{
+class Korisnici extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-     users:[]
+      users: [],
+      pages: [],
+      activePage: 1
     }
   }
   componentWillMount() {
     this.getAllUsers();
   }
+  displayPage = async (page) => {
+    let response = await post.secure('/allUsers', { page: page });
+    if (response.token.success) {
+      let users = response.users.map(item => {
+        return {
+          userId: item.id,
+          userFirstName: item.firstName,
+          userLastName: item.lastName,
+          userEmail: item.email
+        }
+      })
+      this.setState({
+        users,
+        activePage: page
+      })
+    }
+  }
   deleteUser = async (userId) => {
     let response = await post.secure('/deleteUser', {
       userId
-    });
+    })
+    if (response.deleted) {
+      this.displayPage(this.state.activePage)
+    }
   }
-  getAllUsers = async () => {
-    let response = await post.secure('/allUsers',{page:1});
+  displayUsers = async (page) => {
+    let response = await post.secure('/allUsers', { page: page });
     if (response.token.success) {
-      let users=response.users.map(item => {
+      let users = response.users.map(item => {
         return {
-          userId:item.id,
-          userFirstName:item.firstName,
-          userLastName:item.lastName,
-          userEmail:item.email
+          userId: item.id,
+          userFirstName: item.firstName,
+          userLastName: item.lastName,
+          userEmail: item.email
         }
       })
-      this.setState({users})
+      this.setState({
+        users,
+        activePage: page
+      })
+    }
+  }
+  getAllUsers = async () => {
+    let response = await post.secure('/allUsers', { page: this.state.activePage });
+    if (response.token.success) {
+      let users = response.users.map(item => {
+        return {
+          userId: item.id,
+          userFirstName: item.firstName,
+          userLastName: item.lastName,
+          userEmail: item.email
+        }
+      })
       console.log('RESPONSE KORISNICI', response)
+      let pages = []
+      for (let i = 1; i <= response.pages; i++) {
+        let page = {
+          page: i
+        }
+        pages.push(page)
+      }
+      this.setState({
+        users,
+        pages
+      })
     }
   }
   render() {
-    console.log("STATE USERS",this.state.users)
-    return(
+    console.log("STATE USERS", this.state)
+    return (
       <div>
         {
           this.state.users.length ?
@@ -53,7 +102,7 @@ class Korisnici extends React.Component{
                     return (
                       <Table.Row key={item.userId}>
                         <Table.Cell>{item.userId}</Table.Cell>
-                        <Table.Cell>{item.userLastName + " "+item.userFirstName}</Table.Cell>
+                        <Table.Cell>{item.userLastName + " " + item.userFirstName}</Table.Cell>
                         <Table.Cell>{item.userEmail}</Table.Cell>
                         <Table.Cell>
                           <Button icon onClick={() => this.editObj(item.userId)}>
@@ -70,20 +119,21 @@ class Korisnici extends React.Component{
               </Table.Body>
             </Table> : null
         }
-        {/* {
+        {
           this.state.pages.length ?
             <Menu borderless>
               {
                 this.state.pages.map((item, key) => {
                   return (
-                    <Menu.Item name={item.number.toString()}
-                      active={this.state.activeItem === item.number.toString()}
-                      onClick={() => this.categoryObjpageN(item.number)} />
+                    <Menu.Item name={item.page.toString()}
+                      active={this.state.activePage === item.page}
+                      onClick={() => this.displayUsers(item.page)}
+                    />
                   )
                 })
               }
             </Menu> : null
-        } */}
+        }
       </div>
     )
   }
