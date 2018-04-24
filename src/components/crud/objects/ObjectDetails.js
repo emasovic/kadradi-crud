88bab1e646aa18122c2840b22184956dffba2fab
elvis <ekobree@live.com>
2018-04-24 11:12:48 +0200

    preparing to pull

--- src/components/crud/objects/ObjectDetails.js
+++ src/components/crud/objects/ObjectDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Image, Tab, Button, Dropdown, Table, Checkbox } from 'semantic-ui-react';
+import { Grid, Image, Label, Button, Segment, Header } from 'semantic-ui-react';
 import post from '../../fetch/post';
 import { withRouter } from 'react-router';
 
@@ -24,28 +24,65 @@ class ObjectDetails extends Component {
     this.getObjDetails(this.props.match.params.id)
   }
   render() {
-    let photo = Object.keys(this.state.objDetails).length && this.state.objDetails.photos.length ? this.state.objDetails.photos[0].photo_reference : ''
+    let photo = Object.keys(this.state.objDetails).length && typeof this.state.objDetails.photos != 'undefined' ? this.state.objDetails.photos[0].photo_reference : ''
     let src = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + photo + '&key=AIzaSyDImc0NawEJTQwlDskJBSL7cidhVvlccvQ'
+    let openingHours = typeof this.state.objDetails.opening_hours != 'undefined' ? this.state.objDetails.opening_hours.weekday_text : []
     console.log("STEJT", this.state)
     return (
       <div style={{ height: '100vh' }}>
-        <Grid celled='internally'>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              {this.state.objDetails.formatted_address} <br />
-              {this.state.objDetails.formatted_phone_number}
+        <Grid columns='equal'>
+          <Grid.Row stretched>
+            <Grid.Column>
+              <Segment>
+                <Label>
+                  Telefon: {this.state.objDetails.formatted_phone_number}
+                </Label>
+              </Segment>
+              <Segment>
+                <Label>
+                  Adresa: {this.state.objDetails.vicinity}
+                </Label>
+              </Segment>
             </Grid.Column>
-            <Grid.Column centered width={13}>
-              <Image centered src={src} />
-              {this.state.objDetails.name}
+            <Grid.Column width={6}>
+              <Segment>
+                <Header
+                  as='h2'
+                  textAlign='center'
+                  image={this.state.objDetails.icon}
+                  content={this.state.objDetails.name}
+                />
+                <Image centered src={src} />
+              </Segment>
             </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Image src='/assets/images/wireframe/image.png' />
-            </Grid.Column>
-            <Grid.Column width={10}>
-              <Image src='/assets/images/wireframe/paragraph.png' />
+            <Grid.Column>
+              <Segment>
+                <Label>
+                  Website:
+                  <a href={this.state.objDetails.website}>
+                    {this.state.objDetails.website || '/'}
+                  </a>
+                </Label>
+              </Segment>
+              <Segment>
+                <Header
+                  as='h2'
+                  textAlign='center'
+                  content='Radno vreme'
+                />
+                {
+                  openingHours.length ?
+                    openingHours.map((item, key) => {
+                      return (
+                        <div style={{ marginBottom: '2px' }} key={item}>
+                          <Label>
+                            {item}
+                          </Label>
+                        </div>
+                      )
+                    }) : '/'
+                }
+              </Segment>
             </Grid.Column>
           </Grid.Row>
         </Grid>
