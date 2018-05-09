8f58ce723408773dd61d23e2e9db693b6f8d3ab9
Your Name <stefanats@hotmail.com>
2018-05-09 13:59:30 +0200

    stefan

--- kit/entry/server.js
+++ kit/entry/server.js
@@ -937,7 +937,7 @@ const router = (new KoaRouter())
       }
       if (!_.isEmpty(objectPhonesArr)) {
         objectPhonesArr.map(async item => {
-          await db.models.objectPhones.update(item, { where: { id: item.id } })
+          await db.models.objectPhones.create(item)
         })
       }
       if (!_.isEmpty(deletePhones)) {
